ba520a98b386904bc35c859ff6131f1c1e467546
HeyitsAsh <vsashashik@gmail.com>
2022-12-06 20:09:31 +0530

    changed all float rounding to python decimal

--- source/calculations.py
+++ source/calculations.py
@@ -1,4 +1,6 @@
 import math
+import decimal
+from decimal import Decimal
 
 def euler_to_quaternion(yaw: float, #yaw in radian
                         pitch: float, #yaw in radian
@@ -12,10 +14,10 @@ def euler_to_quaternion(yaw: float, #yaw in radian
     sp = math.sin(yaw/2)
     sr = math.sin(roll/2)
 
-    w = round(cy*cp*cr - sy*sp*sr, 12)
-    x = round(sy*sp*cr + cy*cp*sr, 12)
-    y = round(sy*cp*cr + cy*sp*sr, 12)
-    z = round(cy*sp*cr - sy*cp*sr, 12)
+    w = Decimal(str(cy*cp*cr - sy*sp*sr)).__float__()
+    x = Decimal(str(sy*sp*cr + cy*cp*sr)).__float__()
+    y = Decimal(str(sy*cp*cr + cy*sp*sr)).__float__()
+    z = Decimal(str(cy*sp*cr - sy*cp*sr)).__float__()
 
     return w, x, y, z
 
@@ -24,14 +26,20 @@ def quaternion_to_euler(w: float,
                         y: float,
                         z: float):
 
-    if(abs(round(x*y + z*w, 3)) == 0.5):
+    if(abs(Decimal(str(x*y + z*w)).__float__()) == 0.5):
         pitch = (2*(x*y + z*w)) * 2 * math.atan2(x, w)
         roll = 0.0
     else:
         pitch = math.atan2(2*y*w - 2*x*z, 1 - 2*y**2 - 2*z**2)
         roll = math.atan2(2*x*w - 2*y*z, 1 - 2*x**2 - 2*z**2)
     
-    yaw = math.asin(round(2*x*y + 2*z*w, 3))
+    if(Decimal(str(2*x*y + 2*z*w)).__float__() <= Decimal('1.0')):
+        yaw = math.asin(Decimal(str(2*x*y + 2*z*w)).__float__())
+    else:
+        if(Decimal(str(2*x*y + 2*z*w)).__float__() < Decimal('1.0005')):
+            yaw = math.asin(1.0)
+        else:
+            raise ValueError("math domain error - not in [-1,1]")
     
     return yaw, pitch, roll
 
@@ -45,9 +53,9 @@ def quaternion_to_rotmat(w: float,
     # 1 - 2*qy2 - 2*qz2	    2*qx*qy - 2*qz*qw	2*qx*qz + 2*qy*qw
     # 2*qx*qy + 2*qz*qw	    1 - 2*qx2 - 2*qz2	2*qy*qz - 2*qx*qw
     # 2*qx*qz - 2*qy*qw	    2*qy*qz + 2*qx*qw	1 - 2*qx2 - 2*qy2
-    r1 = [round(1 - 2*y**2 - 2*z**2, 12), round(2*x*y - 2*z*w, 12), round(2*x*z + 2*y*w, 12)]
-    r2 = [round(2*x*y + 2*z*w, 12), round(1 - 2*x**2 - 2*z**2, 12), round(2*y*z - 2*x*w, 12)]
-    r3 = [round(2*x*z - 2*y*w, 12), round(2*y*z + 2*x*w, 12), round(1 - 2*x**2 - 2*y**2, 12)]
+    r1 = [Decimal(str(1 - 2*y**2 - 2*z**2)).__float__(), Decimal(str(2*x*y - 2*z*w)).__float__(), Decimal(str(2*x*z + 2*y*w)).__float__()]
+    r2 = [Decimal(str(2*x*y + 2*z*w)).__float__(), Decimal(str(1 - 2*x**2 - 2*z**2)).__float__(), Decimal(str(2*y*z - 2*x*w)).__float__()]
+    r3 = [Decimal(str(2*x*z - 2*y*w)).__float__(), Decimal(str(2*y*z + 2*x*w)).__float__(), Decimal(str(1 - 2*x**2 - 2*y**2)).__float__()]
 
     return [r1,r2,r3]
 
@@ -65,14 +73,15 @@ def euler_to_rotmat(yaw: float, #yaw in radian
     #   ch*ca	                -ch*sa*cb + sh*sb	            ch*sa*sb + sh*cb
     #   sa	                    ca*cb	                        -ca*sb
     #   -sh*ca	                sh*sa*cb + ch*sb	            -sh*sa*sb + ch*cb
-    r1 = [round(ch*ca, 12),     round(-ch*sa*cb + sh*sb, 12),   round(ch*sa*sb + sh*cb, 12)]
-    r2 = [round(sa, 12),        round(ca*cb, 12),               round(-ca*sb, 12)]
-    r3 = [round(-sh*ca, 12),    round(sh*sa*cb + ch*sb, 12),    round(-sh*sa*sb + ch*cb, 12)]
+    r1 = [Decimal(str(ch*ca)).__float__(),     Decimal(str(-ch*sa*cb + sh*sb)).__float__(),   Decimal(str(ch*sa*sb + sh*cb)).__float__()]
+    r2 = [Decimal(str(sa)).__float__(),        Decimal(str(ca*cb)).__float__(),               Decimal(str(-ca*sb)).__float__()]
+    r3 = [Decimal(str(-sh*ca)).__float__(),    Decimal(str(sh*sa*cb + ch*sb)).__float__(),    Decimal(str(-sh*sa*sb + ch*cb)).__float__()]
 
     return [r1,r2,r3]
 
 
 def main(): # Test function
+    decimal.ROUND_05UP
 
     inp = 90 * math.pi / 180
     w,x,y,z = euler_to_quaternion(inp, 0, 0)
